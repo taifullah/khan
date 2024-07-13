70c033488c5783e50896e73be521185f11ce6efa
taifullah <taifullah21@gmail.com>
2024-07-13 17:17:05 +0500

    firs

---
+++ class_1.py
@@ -0,0 +1,11 @@
+message = "hello \"world's is the \"first thing in the python. "
+name :str= "khaan"
+# print(message.title())
+
+
+# print("this is my program"+message+name)
+# print(name.upper())
+# print(name.lower())
+
+# print(name+'  jhjl '+message)
+
